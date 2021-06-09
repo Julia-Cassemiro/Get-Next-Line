1f67a3f1dbd42ca82e9c67f894f817920dfab745
Julia-Cassemiro <jg-cassemiro@hotmail.com>
2021-06-08 23:26:50 -0300

    gnl

---
+++ get_next_line.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*																			*/
+/*														:::	  ::::::::   */
+/*   get_next_line.c									:+:	  :+:	:+:   */
+/*													+:+ +:+		 +:+	 */
+/*   By: jgomes-c <jgomes-c@student.42sp.org.br>	+#+  +:+	   +#+		*/
+/*												+#+#+#+#+#+   +#+		   */
+/*   Created: 2021/06/08 04:13:00 by jgomes-c		  #+#	#+#			 */
+/*   Updated: 2021/06/08 05:03:29 by jgomes-c		 ###   ########.fr	   */
+/*																			*/
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_save_the_next(char *s)
+{
+	int		i;
+	int		count;
+	char	*str;
+
+	i = 0;
+	count = 0;
+	while (s[i] && s[i] != '\n')
+		i++;
+	if (!s[i])
+	{
+		free(s);
+		return (0);
+	}
+	if (!(str = malloc(sizeof(char) * (ft_strlen(s) + 1))))
+		return (0);
+	i++;
+	while (s[i])
+		str[count++] = s[i++];
+	str[count] = '\0';
+	free(s);
+	return (str);
+}
+
+char	*ft_line(char *s)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i] && s[i] != '\n')
+		i++;
+	if (!(str = malloc(sizeof(char) * (i + 1))))
+		return (0);
+	i = 0;
+	while (s[i] && s[i] != '\n')
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+int	get_next_line(int fd, char **line)
+{
+	char		*buff;
+	static char	*save;
+	int			count;
+
+	count = 1;
+	if (fd < 0 || !line || BUFFER_SIZE <= 0)
+		return (-1);
+	if (!(buff = malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+		return (-1);
+	while (ft_verific_newline(save) != 1 && count != 0)
+	{
+		if ((count = read(fd, buff, BUFFER_SIZE)) == -1)
+		{
+			free(buff);
+			return (-1);
+		}
+		buff[count] = '\0';
+		save = ft_strjoin(save, buff);
+	}
+	free(buff);
+	*line = ft_line(save);
+	save = ft_save_the_next(save);
+	if (count == 0)
+		return (0);
+	return (1);
+}
---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*																			*/
+/*														:::	  ::::::::   */
+/*   get_next_line.h									:+:	  :+:	:+:   */
+/*													+:+ +:+		 +:+	 */
+/*   By: jgomes-c <jgomes-c@student.42sp.org.br>	+#+  +:+	   +#+		*/
+/*												+#+#+#+#+#+   +#+		   */
+/*   Created: 2021/06/08 05:00:38 by jgomes-c		  #+#	#+#			 */
+/*   Updated: 2021/06/08 23:23:35 by jgomes-c		 ###   ########.fr	   */
+/*																			*/
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+int			ft_next_line(int fd, char **line);
+int			ft_verific_newline(char *str);
+size_t		ft_strlen(const char *str);
+void		*ft_memmove(void *dst, const void *src, size_t len);
+char		*ft_strjoin(char const *s1, char const *s2);
+
+#endif
