da5b97165e1d9a5817e2f3fd517c4d1f01f09e72
Julia-Cassemiro <jg-cassemiro@hotmail.com>
2021-06-09 12:28:50 -0300

    bonus

---
+++ get_next_line_bonus.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgomes-c <jgomes-c@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/09 12:27:57 by jgomes-c          #+#    #+#             */
+/*   Updated: 2021/06/09 12:28:13 by jgomes-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_save_the_next(char *s)
+{
+	int		i;
+	int		count;
+	char	*str;
+
+	i = 0;
+	count = 0;
+	while (s[i] && s[i] != '\n')
+		i++;
+	if (!s[i])
+	{
+		free(s);
+		return (0);
+	}
+	str = malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (!str)
+		return (0);
+	i++;
+	while (s[i])
+		str[count++] = s[i++];
+	str[count] = '\0';
+	free(s);
+	return (str);
+}
+
+char	*ft_line(char *s)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i] && s[i] != '\n')
+		i++;
+	str = malloc(sizeof(char) * (i + 1));
+	if (!str)
+		return (0);
+	i = 0;
+	while (s[i] && s[i] != '\n')
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+int	get_next_line(int fd, char **line)
+{
+	char		buff[BUFFER_SIZE + 1];
+	static char	*save;
+	int			count;
+
+	count = 1;
+	if (fd < 0 || !line || BUFFER_SIZE <= 0)
+		return (-1);
+	while (ft_verific_newline(save) != 1 && count != 0)
+	{
+		count = read(fd, buff, BUFFER_SIZE);
+		if (count == -1)
+			return (-1);
+		buff[count] = '\0';
+		save = ft_strjoin(save, buff);
+	}
+	*line = ft_line(save);
+	save = ft_save_the_next(save);
+	if (count == 0)
+		return (0);
+	return (1);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgomes-c <jgomes-c@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/09 12:28:18 by jgomes-c          #+#    #+#             */
+/*   Updated: 2021/06/09 12:28:21 by jgomes-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+int			get_next_line(int fd, char **line);
+int			ft_verific_newline(char *str);
+size_t		ft_strlen(const char *str);
+void		*ft_memmove(void *dst, const void *src, size_t len);
+char		*ft_strjoin(char const *s1, char const *s2);
+
+#endif
