2e16223212520299cb7c71456dca4a6838b1cf1d
Julia-Cassemiro <jg-cassemiro@hotmail.com>
2021-06-09 16:13:54 -0300

    tmp

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jgomes-c <jgomes-c@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/09 12:27:57 by jgomes-c          #+#    #+#             */
-/*   Updated: 2021/06/09 16:12:20 by jgomes-c         ###   ########.fr       */
+/*   Updated: 2021/06/09 16:13:44 by jgomes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,13 +68,12 @@ int	get_next_line(int fd, char **line)
 	char		*buff;
 	static char	*save[OPEN_MAX];
 	int			count;
-	char		*tmp;
 
 	buff = ft_verific_buff(fd, line);
 	if (!buff)
 		return (-1);
 	count = 1;
-	while (ft_verific_newline(save[fd]) != 1 && count != 0)
+	while (ft_verific_newline(save) != 1 && count != 0)
 	{
 		count = read(fd, buff, BUFFER_SIZE);
 		if (count == -1)
@@ -83,12 +82,11 @@ int	get_next_line(int fd, char **line)
 			return (-1);
 		}
 		buff[count] = '\0';
-		tmp = ft_strjoin(save[fd], buff);
-		save[fd] = tmp;
+		save = ft_strjoin(save, buff);
 	}
 	free(buff);
-	*line = ft_line(save[fd]);
-	save[fd] = ft_save_the_next(save[fd]);
+	*line = ft_line(save);
+	save = ft_save_the_next(save);
 	if (count == 0)
 		return (0);
 	return (1);
